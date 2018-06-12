203d4faece3e0c1cbf0cc742e63e47e240d79004
Grahm Manley <grahm.manley@gmail.com>
2018-06-12 09:05:06 -0500

    tests user object

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "validations" do
+        let (:user) { User.new(username: "testuser", email: "test@getflywheel.com", password: "123456xX$", salt: "1234" ) }
+	context "for presence of data in certain fields" do
+	    it "is valid with valid attributes" do
+	    	expect(user).to be_valid
+	    end
+	    it "is not valid with invalid attributes"			
+            it "is not valid without a username" 
+            it "is not valid without a email"
+            it "is not valid without a salt"
+            it "is not valid without a password"
+	 end
+    end
 end
