701a8748c70451eef746e4f539747aed24ccb415
Neil Koul <neilkant@hotmail.com>
2018-06-15 10:02:22 -0500

    Add untracked files for mailer

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe UserMailer, type: :mailer do
+  describe "accout_activation" do
+    let(:mail) { UserMailer.accout_activation }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Accout activation")
+      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Hi")
+    end
+  end
+
+  describe "password_reset" do
+    let(:mail) { UserMailer.password_reset }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password reset")
+      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Hi")
+    end
+  end
+
+end
