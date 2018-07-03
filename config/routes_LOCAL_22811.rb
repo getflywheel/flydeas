5a00514beb142c826f8fadb7e76efb182e09651d
Neil Koul <neilkant@hotmail.com>
2018-07-03 13:09:17 -0500

    Forms stylized

--- config/routes_LOCAL_22811.rb
+++ config/routes_LOCAL_22811.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
+  resources :submissions
   root :to =>'submissions#index'
-  resources :submissions do
-     resources :votes
-    end
+  resources :submissions
+
+  resources :account_activations, only: [:edit] do
+  end
   default_url_options :host => "localhost"
+
+  resources :logins, only: %i(new create) do
+  end
   resources :password_resets, only: [:new, :create, :edit, :update]
+
   # Log in and log out routes
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :logins, only: %i(new create)
+  resources :logins, only: %i(new create) do
+  end
+
 end
